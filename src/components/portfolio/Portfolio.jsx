dcc6bd903a00cae47b8f6f4b9bb7d103b41d0cb6
Thiem <thiem.huynh0@gmail.com>
2023-07-31 21:07:27 -0400

    portfolio content generation -use github api later

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './portfolio.css'
-import IMG1 from '../../assets/web1.png'
-import IMG2 from '../../assets/web2.png'
+import data from './portfolio-sample'
 
 const Portfolio = () => {
   return (
@@ -10,7 +9,27 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container ">
-        <article className="portfolio__item">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                  <h3>{title}<h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
+                  <div className="portfolio__item-cta">
+                    <a href={github} className="btn" target='_blank' rel="noreferrer">Github</a>
+                    <a href={demo} className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
+                  </div>
+                </div>
+              </article>
+            )
+          })
+        }
+
+
+
+
+        {/* <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="portfolio sample1" />
             <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
@@ -19,57 +38,8 @@ const Portfolio = () => {
               <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
             </div>
           </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="portfolio sample2" />
-            <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/business-thiem/project2-doggo-quiz" className="btn" target='_blank' rel="noreferrer">Github</a>
-              <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
-            </div>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="portfolio sample1" />
-            <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/business-thiem/project2-doggo-quiz" className="btn" target='_blank' rel="noreferrer">Github</a>
-              <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
-            </div>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="portfolio sample2" />
-            <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/business-thiem/project2-doggo-quiz" className="btn" target='_blank' rel="noreferrer">Github</a>
-              <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
-            </div>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="portfolio sample1" />
-            <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/business-thiem/project2-doggo-quiz" className="btn" target='_blank' rel="noreferrer">Github</a>
-              <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
-            </div>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="portfolio sample2" />
-            <h3>This is a portfolio item <h6>(TODO: place link to the actual demo when hosting is online)</h6> </h3>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/business-thiem/project2-doggo-quiz" className="btn" target='_blank' rel="noreferrer">Github</a>
-              <a href="#" className='btn btn-primary' target='_blank'  rel="noreferrer">Live Demo</a>
-            </div>
-          </div>
-        </article>
+        </article> */}
+
       </div>
     </section>
   )
---
+++ src/components/portfolio/portfolio-sample.jsx
@@ -0,0 +1,52 @@
+import IMG1 from '../../assets/web1.png'
+import IMG2 from '../../assets/web2.png'
+
+
+// this is a test of markup auto generation TODO: make a github API connection to generate this later.
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Doggo Quiz',
+        github: 'https://github.com/business-thiem/project2-doggo-quiz',
+        demo: 'https://github.com/business-thiem/project2-doggo-quiz'
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'Tic Tac Toe Game',
+        github: 'https://github.com/business-thiem/tictactoe-game',
+        demo: 'https://github.com/business-thiem/tictactoe-game'
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: 'Doggo Quiz',
+        github: 'https://github.com/business-thiem/project2-doggo-quiz',
+        demo: 'https://github.com/business-thiem/project2-doggo-quiz'
+    },
+    {
+        id: 4,
+        image: IMG2,
+        title: 'Tic Tac Toe Game',
+        github: 'https://github.com/business-thiem/tictactoe-game',
+        demo: 'https://github.com/business-thiem/tictactoe-game'
+    },
+    {
+        id: 5,
+        image: IMG1,
+        title: 'Doggo Quiz',
+        github: 'https://github.com/business-thiem/project2-doggo-quiz',
+        demo: 'https://github.com/business-thiem/project2-doggo-quiz'
+    },
+    {
+        id: 6,
+        image: IMG2,
+        title: 'Tic Tac Toe Game',
+        github: 'https://github.com/business-thiem/tictactoe-game',
+        demo: 'https://github.com/business-thiem/tictactoe-game'
+    }
+
+]
+
+export default data;
