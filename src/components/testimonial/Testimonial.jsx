a19cf5b3a1d9cd8de17b7d7fdce0946ab222ff66
Thiem <thiem.huynh0@gmail.com>
2023-08-07 15:29:30 -0400

    my custom code contributions to testimonial compo

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import './testimonial.css'
-
+import reviewData from './testimonial-sample.jsx'
 import images from './dynamic-imageImport.jsx'
 images.pop(); //deletes the empty last element
-
-const imgList = Object.keys(images).map(key => (
-  <img src={images[key]} className="avatar__image" alt={key} />
-))
 
 const Testimonial = () => {
   return (
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="avatar-img__container">
-        {imgList}
+      <div className="container testimonials__container">
+        {
+          reviewData.map(({id, clientName, comment}) => {
+            return (
+              <article className="testimonial">
+                <div className="client__avatar">
+                  <img src={images[id]} className="avatar__image" alt={"avatar image " + id + " of " + clientName} />
+                </div>
+                <h5 className="client__name">{clientName}</h5>
+                <small className='client__review'>
+                  {comment}
+                </small>
+              </article>
+
+            )
+          })
+        }
       </div>
-
-
-
-
-      
     </section>
   )
 }
---
+++ src/components/testimonial/testimonial-sample.jsx
@@ -0,0 +1,31 @@
+const reviewData = [
+
+    {
+        id: 0,
+        clientName: 'Anita Bath',
+        comment: "Thiem is the best developer. He did everything I asked for. He even wrote this testimonial about himself. I have never seen so much brilliant ingenuity and self motivation. It was as though he was the one writing this all along. This was totally better than lorem ipsum dolor. Check out the other comments!"
+    },
+    {
+        id: 1,
+        clientName: "Barry Screws",
+        comment: "That's it mister! you just lost your brain priviliges! squidward... i used your clarinet to unclog my toilet! squidward, you're steaming. you're like a steamed vegetable, only smarter. you're a man now, spongebob, and it's time you started acting like one. the maniacs in the mailbox! squidward? squidward! "
+    },
+    {
+        id: 2,
+        clientName: "Liz Anya",
+        comment: "If i were to die right now in a fiery explosion due to carelessness of a friend... . then it would just be alright. good people don't rip other people's arms off. insurance is what i need for a crabby patty. don't we look vicious and bloodthirsty? try one of those radioactive sludge balls you call food?"
+    },
+    {
+        id: 3,
+        clientName: "Fredrick Bars",
+        comment: "Remember, licking doorknobs is illegal on other planets. that's no reason to be ripping people's heads off boy! holographic meatloaf? my favorite! excuse me sir? you're sitting on my face... which is also my body. oh, tartar sauce. the sky had a baby from my cereal box! can i have everybody's attention? i have to use the bathroom."
+    },
+    {
+        id: 4,
+        clientName: "Brandy Peaks",
+        comment: "Spongebob is the only guy i know who can have fun with a jellyfish, for twelve hours! the line for the tunnel of glove is filling up. go out and get yourself a case of the krabbies. mr. krabs, please. i'll prove i'm a fry cook. ask squidward, he'll vouch for me. hey look, a cardboard box washed up on the beach. holy fish paste, it's a guy!!!"
+    }
+
+]
+
+export default reviewData;
