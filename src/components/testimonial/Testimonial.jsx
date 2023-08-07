429c0bfa4c1103f0feae42036982b4c37118e42c
Thiem <thiem.huynh0@gmail.com>
2023-08-07 15:48:45 -0400

    testimonial complete

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import './testimonial.css'
+
+//SWIPER IMPORT -------------  import Swiper core and required modules
+import {  Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+//END SWIPER IMPORT
+
 import reviewData from './testimonial-sample.jsx'
 import images from './dynamic-imageImport.jsx'
+
+
+
 images.pop(); //deletes the empty last element
 
 const Testimonial = () => {
@@ -9,11 +23,18 @@ const Testimonial = () => {
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+          // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}>
+
         {
           reviewData.map(({id, clientName, comment}) => {
             return (
-              <article key={id} className="testimonial">
+              <SwiperSlide key={id} className="testimonial">
                 <div className="client__avatar">
                   <img src={images[id]} className="avatar__image" alt={"avatar image " + id + " of " + clientName} />
                 </div>
@@ -21,12 +42,12 @@ const Testimonial = () => {
                 <small className='client__review'>
                   {comment}
                 </small>
-              </article>
+              </SwiperSlide>
 
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
